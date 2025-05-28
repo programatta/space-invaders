a73c6525b1e03099efb704df158e4b1c7369c2aa
programatta <programatta@gmail.com>
2025-05-28 13:07:19 +0200

    dibujo en ventana con primitiva

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
 
 type Game struct{}
 
@@ -10,6 +15,8 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0x03, 0x04, 0x5e, 0xFF})
+	vector.DrawFilledRect(screen, 100, 100, 120, 120, color.RGBA{0, 255, 0, 255}, true)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
